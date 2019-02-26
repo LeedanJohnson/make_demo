3cb66245c79818475d692d62f8b3eb994c3969ad
Leedan Johnson <leedanjohnson@Gmail.com>
2019-02-26 13:43:32 -0700

    Started creating demo

---
+++ lib/custommath/custommath.hpp
@@ -0,0 +1,10 @@
+#ifndef _CUSTOMMATH_HPP_
+#define _CUSTOMMATH_HPP_
+
+#include <string>
+
+namespace custommath {
+    add(std::string first, std::string second);
+}
+
+#endif // _CUSTOMMATH_HPP_
---
+++ lib/customstring/customstring.hpp
@@ -0,0 +1,10 @@
+#ifndef _CUSTOMSTRING_HPP_
+#define _CUSTOMSTRING_HPP_
+
+#include <string>
+
+namespace customstring {
+    concat(std::string first, std::string second);
+}
+
+#endif // _CUSTOMSTRING_HPP_
